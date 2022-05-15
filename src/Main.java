b8cd4c97ed11c2eefc3c1eef7f5d6ec8b7fb4f7d
sadettin uysal <tubasariz@gmail.com>
2022-05-15 11:11:50 -0400

    the basic structure of the car is ready, working on advance features

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.company.basicFeatures;
+
+public class Main {
+
+    public static void main (String[] args){
+        basicFeatures car = new basicFeatures();
+        
+    }
+
+}
---
+++ src/com/company/basicFeatures.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class basicFeatures {
+    String engineCapac;
+    String Color;
+    String doors;
+    String headlights;
+    String regNo;
+
+}
